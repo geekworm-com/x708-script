61f79fd785666c330d5c090b05575aab0c73eebc
huangyp <harry@geekworm.com>
2024-03-22 17:41:42 +0800

    update

---
+++ uninstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Uninstall x708 installation script
+sudo systemctl stop x708-pwr
+sudo systemctl disable x708-pwr
+file_path="/lib/systemd/system/x708-pwr.service"
+if [ -f "$file_path" ]; then    
+    sudo rm -f "$file_path"    
+fi
+
+file_path="/usr/local/bin/x708-pwr.sh"
+if [ -f "$file_path" ]; then    
+    sudo rm -f "$file_path"    
+fi
+
+file_path="/usr/local/bin/xgpio_pwr"
+
+if [ -f "$file_path" ]; then    
+    sudo rm -f "$file_path"    
+fi
+
+# Remove the xoff allias on .bashrc file
+sudo sed -i '/x708off/d' ~/.bashrc
+source ~/.bashrc
+
+file_path="/usr/local/bin/x708-softsd.sh"
+if [ -f "$file_path" ]; then    
+    sudo rm -f "$file_path"    
+fi
+
+file_path="/usr/local/bin/xgpio_soft"
+if [ -f "$file_path" ]; then    
+    sudo rm -f "$file_path"    
+fi
